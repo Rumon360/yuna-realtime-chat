830efc3fe2f885bfc5f272ad1b44141811ceb5ec
Rumon360 <hmk.rumon@gmail.com>
2026-04-05 00:22:40 +0600

    chore: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,18 @@ import { Space_Grotesk } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils"
+import { Metadata } from "next"
 
 const SpaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   variable: "--font-sans",
 })
+
+export const metadata: Metadata = {
+  title: "Yuna",
+  description:
+    "Yuna is a lightweight, real-time anonymous chat app where users can chat by sharing a unique link. Chats automatically self-destruct 10 minutes after creation, ensuring privacy and temporary conversations.",
+}
 
 export default function RootLayout({
   children,
